c57e5d7d0ea523ad347f44cd69d4882c527462e1
paulvee <pw.versteeg@gmail.com>
2026-03-22 13:30:05 +0100

    Clean Batt-Test-App folder: remove all previous Batt Tester app files and binaries in preparation for fresh v2.1.2 import

--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core business logic package for Battery Tester
-"""
-
-from .models import TestState, TestParameters, DataPoint, TestData
-
-__all__ = ['TestState', 'TestParameters', 'DataPoint', 'TestData']
--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions package for Battery Tester
-"""
-
-from .config_manager import ConfigManager
-from .export_utils import ExportManager
-
-__all__ = ['ConfigManager', 'ExportManager']
