565c9c6375a7cc81fddacece585f13dff56e18e1
paulvee <pw.versteeg@gmail.com>
2026-03-22 13:32:23 +0100

    Delete Batt-Test-App directory

--- Batt-Test-App/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-"""Battery Tester Application
-
-A Python/PyQt6 application for battery discharge testing and analysis.
-"""
-
-__version__ = "2.1.1"
-__author__ = "Paul Versteeg & Bud Bennett with the help of agent Claude Sonnet 4.5"
-__original_author__ = "John Lowen (in memory) for the Delphi-7 version"
